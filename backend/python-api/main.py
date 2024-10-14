c3c9473f6d2e46f305d1b23f95ec1fe65e00e855
Barnabas Somodi <79094366+Barnabas35@users.noreply.github.com>
2024-10-14 20:13:05 +0100

    CORS Error Fix

--- backend/python-api/main.py
+++ backend/python-api/main.py
@@ -3,9 +3,10 @@
 # Database queries are not handled in this script.
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def index():
