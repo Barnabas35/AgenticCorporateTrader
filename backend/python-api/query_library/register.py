18448a494381dd7cd901f5649bf19c86510e5ce9
Barnabas Somodi <79094366+Barnabas35@users.noreply.github.com>
2024-10-19 17:51:51 +0100

    Expanded Registration
    Now registration requires a user type of admin or fa or fm.

--- backend/python-api/query_library/register.py
+++ backend/python-api/query_library/register.py
@@ -7,18 +7,27 @@ from function_library.string_generator import new_session_token
 def q_register(request_json):
 
     # Make sure username and password are not empty
-    if "username" not in request_json or "password" not in request_json or "email" not in request_json:
-        return {"status": "No username or password or email provided."}
+    if ("username" not in request_json
+            or "password" not in request_json
+            or "email" not in request_json
+            or "user_type" not in request_json):
+        return {"status": "No username or password or email or user type provided."}
 
     # Get username and password from request
     username = request_json["username"]
     password = request_json["password"]
     email = request_json["email"]
+    user_type = request_json["user_type"]
 
     # Parse username and password to be safe for Firestore
     username = firestore_safe(username)
     password = firestore_safe(password)
     email = firestore_safe(email)
+    user_type = firestore_safe(user_type)
+
+    # Check that user type is valid [admin, fund administrator (fa), fund manager (fm)]
+    if user_type != "admin" and user_type != "fa" and user_type != "fm":
+        return {"status": "Invalid user type."}
 
     # Get database reference
     db = DBAccess.get_db()
@@ -41,7 +50,11 @@ def q_register(request_json):
     password_hashed = sha256(password.encode("utf-8")).hexdigest()
 
     # Create new user
-    db.collection("users").add({"username": username, "password": password_hashed, "email": email, "session_token": ""})
+    db.collection("users").add({"username": username,
+                                "password": password_hashed,
+                                "email": email,
+                                "user_type": user_type,
+                                "session_token": ""})
 
     # Return status
     return {"status": "Success"}
