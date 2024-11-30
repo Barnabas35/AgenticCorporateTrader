075ba7e491ab7de37c27842a141ead7e26660118
Barnabas Somodi <79094366+Barnabas35@users.noreply.github.com>
2024-11-30 19:16:58 +0000

    Email Password Security

--- backend/python-api/function_library/send_email.py
+++ backend/python-api/function_library/send_email.py
@@ -1,6 +1,7 @@
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import smtplib
+import os
 
 
 def send_email_aws(dest_email, from_email, subject, body):
@@ -40,10 +41,23 @@ def send_email_aws(dest_email, from_email, subject, body):
 
 def send_email_smtp(dest_email, subject, body):
 
+    # Get Gmail app password from environment variable
+    GMAIL_APP_PASSWORD_ENV = os.getenv("GMAIL_APP_PASSWORD")
+
+    # If GMAIL_APP_PASSWORD_ENV environment variable is not set, use fallback config.txt line 6
+    if GMAIL_APP_PASSWORD_ENV is None:
+        try:
+            with open("config.txt", "r") as f:
+                GMAIL_APP_PASSWORD_ENV = f.readlines()[5].strip("\n")
+                f.close()
+
+        except FileNotFoundError:
+            return {"status": "No Gmail app password found."}
+
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login("tradeagently@gmail.com", "kjlnxgxcvhwefmep")
+        server.login("tradeagently@gmail.com", GMAIL_APP_PASSWORD_ENV)
         message = 'Subject: {}\n\n{}'.format(subject, body)
         server.sendmail("tradeagently@gmail.com", dest_email, message)
         server.quit()
